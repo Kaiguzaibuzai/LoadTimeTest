9d02e4f7181671acafb64106bd4565766f41c341
Kaiguzaibuzai <82721366+Kaiguzaibuzai@users.noreply.github.com>
2021-04-30 15:16:46 +0800

    Add files via upload

---
+++ LoadGltfJSZip/QuickSort.js
@@ -0,0 +1,41 @@
+function quickSort(arr, left, right) {
+    
+   
+    var len = arr.length,
+        partitionIndex,
+        left = typeof left !== 'number' ? 0 : left,
+        right = typeof right !== 'number' ? len - 1 : right;
+
+    if (left < right) {
+
+        partitionIndex = partition(arr, left, right);
+        quickSort(arr, left, partitionIndex - 1);
+        quickSort(arr, partitionIndex + 1, right);
+    }
+
+    return arr;
+}
+
+function partition(arr, left, right) {
+
+    var pivot = left,
+        index = pivot + 1;
+
+    for (var i = index; i <= right; i++) {
+
+        if (arr[i] < arr[pivot]) {
+            swap(arr, i, index);
+            index++;
+        }
+    }
+ 
+    swap(arr, pivot, index - 1);
+
+    return index - 1;
+}
+
+function swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
